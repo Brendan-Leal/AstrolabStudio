64eae9ddc2cecbbf1a9c7213d8de63deff1b28df
Brendan Leal <leal.brendan@gmail.com>
2023-04-18 11:40:33 -0700

    add profile pictures

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Head from 'next/head';
+import Image from 'next/image';
 import ContactForm from '@/components/ContactForm';
+import brendanPfp from '../../public/brendan-pfp.png';
+import marisaPfp from '../../public/marisa-pfp.png';
 import style from '../styles/index.module.css';
 
 export default function LandingPage() {
@@ -15,52 +18,59 @@ export default function LandingPage() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
-      <main>
-        <section className={style.intro}>
-          <div className={style.contentContainer}>
-            <h2>Your Passion, Our Mission</h2>
-            <p>
-              From web development to digital strategy, curating and building an
-              online presence is just the beginning. We&apos;re here to help every step
-              of the journey.
-            </p>
-            <div className={style.headshots}>
-              {/* TODO images here */}
-            </div>
+      <section className={style.container}>
+        <div className={style.contentContainer}>
+          <h2>Your Passion, Our Mission</h2>
+          <p>
+            From web development to digital strategy, curating and building an
+            online presence is just the beginning. We&apos;re here to help every step
+            of the journey.
+          </p>
+          <div className={style.headshots}>
+            <Image
+              src={marisaPfp}
+              width={150}
+              height={150}
+              alt="Marisa headshot"
+              className={style.pfp}
+            />
+            <Image
+              src={brendanPfp}
+              width={150}
+              height={150}
+              alt="Brendan headshot"
+              className={style.pfp}
+            />
           </div>
-          <div className={style.contentContainer}>
-            <h2>Welcome Aboard</h2>
-            <p>
-              Hi, we&apos;re Marisa and Brendan Leal. Brother and sister duo with a
-              shared passion of all things online culture. From dial-up in our
-              childhood home, to AI in our pockets, we know the internet can be a
-              wonderful and whacky place.
-            </p>
-            <br />
-            <p>
-              From creating websites to content strategy,
-              we&apos;re here to help you take off, online.
-            </p>
-          </div>
-          <div className={style.contentContainer}>
-            <h2>How We Can Help</h2>
-            <ul>
-              <li>Web Development</li>
-              <li>e-Commerce</li>
-              <li>Content & Copywriting</li>
-              <li>Social Media Strategy</li>
-              <li>Content Strategy</li>
-              <li>Paid & Social Advertising</li>
-              <li>Brand Awareness & Development</li>
-              <li>Email Marketing</li>
-            </ul>
-          </div>
-        </section>
-        <ContactForm />
-      </main>
-      <footer className={style.footer}>
-        <div className={style.semiCircle} />
-      </footer>
+        </div>
+        <div className={style.contentContainer}>
+          <h2>Welcome Aboard</h2>
+          <p>
+            Hi, we&apos;re Marisa and Brendan Leal. Brother and sister duo with a
+            shared passion of all things online culture. From dial-up in our
+            childhood home, to AI in our pockets, we know the internet can be a
+            wonderful and whacky place.
+          </p>
+          <br />
+          <p>
+            From creating websites to content strategy,
+            we&apos;re here to help you take off, online.
+          </p>
+        </div>
+        <div className={style.contentContainer}>
+          <h2>How We Can Help</h2>
+          <ul>
+            <li>Web Development</li>
+            <li>e-Commerce</li>
+            <li>Content & Copywriting</li>
+            <li>Social Media Strategy</li>
+            <li>Content Strategy</li>
+            <li>Paid & Social Advertising</li>
+            <li>Brand Awareness & Development</li>
+            <li>Email Marketing</li>
+          </ul>
+        </div>
+      </section>
     </>
   );
 }
