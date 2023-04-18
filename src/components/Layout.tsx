d2d556e85c7f391fc41230a64a946e54469030a5
Brendan Leal <leal.brendan@gmail.com>
2023-04-18 11:41:10 -0700

    add google font Poppins

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,17 @@
 import React, { ReactElement } from 'react';
+import { Poppins } from 'next/font/google';
 import style from '../styles/layout.module.css';
 import Footer from './Footer';
 import Header from './Header';
 
+const poppins = Poppins({
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+});
+
 export default function Layout({ children }: { children: ReactElement }) {
   return (
-    <div className={style.layout}>
+    <div className={`${style.layout} ${poppins.className}`}>
       <Header />
       <main>{children}</main>
       <Footer />
