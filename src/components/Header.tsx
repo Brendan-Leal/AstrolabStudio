aa8abf66aa849f146e710c28d52ff4b839d6b1f4
Brendan Leal <leal.brendan@gmail.com>
2023-04-28 10:59:43 -0700

    add nav, show user which path they are currently on

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import style from '../styles/header.module.css';
 
 export default function Header() {
+  const nextRouter = useRouter();
+  const [currentPath, setCurrentPath] = useState(nextRouter.pathname);
+  const setCurrentNavStyle = (path: string) => (path === currentPath ? 'activeNav' : '');
+
+  useEffect(() => {
+    setCurrentPath(nextRouter.pathname);
+  }, [nextRouter.pathname]);
   return (
-    <header className={style.header}>
-      <div>
-        <h1>Astrolab Studio</h1>
-        <p>Finding your space online</p>
-      </div>
-      <div className={style.planet}>&#x1FA90;</div>
-    </header>
+    <>
+      <header className={style.header}>
+        <div>
+          <h1><Link href="/">Astrolab Studio</Link></h1>
+          <p>Finding your space online</p>
+        </div>
+        <div className={style.planet}>&#x1FA90;</div>
+      </header>
+
+      <nav>
+        <Link
+          href="/"
+          className={setCurrentNavStyle('/')}
+        >
+          Home
+        </Link>
+        <Link
+          href="testimonials"
+          className={setCurrentNavStyle('/testimonials')}
+        >
+          Testimonials
+        </Link>
+      </nav>
+    </>
   );
 }
