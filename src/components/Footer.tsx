eaba26cafc9ccdb132f19e4eafd0f91972c0e268
Brendan Leal <leal.brendan@gmail.com>
2023-05-03 14:18:50 -0700

    use footerContext and set footerRef on the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import FooterContext from '@/Context/FooterContext';
 import styles from '../styles/footer.module.css';
 import ContactForm from './ContactForm';
 
 export default function Footer() {
+  const footerRef = useContext(FooterContext);
+
   return (
-    <footer>
+    <footer ref={footerRef}>
       <ContactForm />
       <div className={styles.footer}>
         <div className={styles.semiCircle} />
