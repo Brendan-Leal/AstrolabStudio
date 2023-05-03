a5d2081b96e76cab72231b29b46ee887e75549dd
Brendan Leal <leal.brendan@gmail.com>
2023-05-03 14:34:38 -0700

    placeholder text

--- src/pages/testimonials.tsx
+++ src/pages/testimonials.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function TestimonialsPage() {
-  return <h1>Testimonials</h1>;
+  return (
+    <>
+      <h1>Testimonials</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nesciunt id ex ad animi omnis, repudiandae quasi, ea, dolore placeat quibusdam cumque? Unde mollitia eveniet alias inventore aliquam dolorum saepe!</p>
+    </>
+  );
 }
