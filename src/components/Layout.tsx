ec2ba5a1c39288a1c2d2cfd98e88774de7a9e4fa
Brendan Leal <leal.brendan@gmail.com>
2023-05-16 13:16:01 -0700

    wrap layout in background color

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google';
 import Footer from './Footer';
 import Header from './Header';
 import FooterContext from '../Context/FooterContext';
+import style from '../styles/layout.module.css';
 
 const poppins = Poppins({
   weight: ['500', '700'],
@@ -13,11 +14,13 @@ export default function Layout({ children }: { children: ReactElement }) {
   const footerRef = useRef<HTMLFormElement>(null);
 
   return (
-    <div className={`${poppins.className}`}>
+    <div className={`${poppins.className} `}>
       <FooterContext.Provider value={footerRef}>
         <Header />
-        <main>{children}</main>
-        <Footer />
+        <div className={style.background}>
+          <main>{children}</main>
+          <Footer />
+        </div>
       </FooterContext.Provider>
     </div>
   );
