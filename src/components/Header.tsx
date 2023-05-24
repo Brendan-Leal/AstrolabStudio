0a2cfc1745877b0dcf354ed97aaed258c159b356
Brendan Leal <leal.brendan@gmail.com>
2023-05-24 14:46:24 -0700

    add logo image, remove planet emoji

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 import style from '../styles/header.module.css';
+import logo from '../../public/Astrolab_Studio_Logo_transparent.png';
 
 export default function Header() {
   return (
     <header className={style.header}>
-      <div>
-        <h1>Astrolab Studio</h1>
-        <p>Finding your space online</p>
-      </div>
-      <div className={style.planet}>&#x1FA90;</div>
+      <Image
+        src={logo}
+        alt="logo"
+        className={style.logo}
+      />
+      <p>Finding your space online</p>
     </header>
   );
 }
